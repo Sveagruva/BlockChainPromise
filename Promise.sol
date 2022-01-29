430114b03fa6ebe3cfd91f559de568339af5ebc8
Sveagruva <55844843+Sveagruva@users.noreply.github.com>
2022-01-29 20:36:57 +0400

    store checkers array && add getVotes function

--- Promise.sol
+++ Promise.sol
@@ -10,6 +10,7 @@ contract Promise {
 
     uint256 expirationTime;
 
+    address[] checkers;
     mapping(address => Vote) results;
 
     constructor(address _goodPromise, address _badPromise, address[] memory _checkers, uint8 _numberOfCheckersToPass, uint256 _expirationTime) {
@@ -25,7 +26,10 @@ contract Promise {
 
         for (uint i = 0; i < _checkers.length; i++) {
             require(_checkers[i] != _goodPromise, "giver of promise cannot validate its own promise");
+            require(results[_checkers[i]] == Vote.notAllowed, "one address can only represent one vote");
+
             results[_checkers[i]] = Vote.notGiven;
+            checkers.push(_checkers[i]);
         }
     }
 
@@ -51,6 +55,14 @@ contract Promise {
 
         if(numberOfCheckers - numberOfCheckersVoted + numberOfCheckersGoodVote < numberOfCheckersToPass) {
             selfdestruct(payable(badPromise));
+        }
+    }
+
+    function getVotes() external view returns (address[] memory checkersList, Vote[] memory checkersVotes) {
+        require(address(0) == msg.sender, "do not write this as a transaction and call it with 0x0 address");
+        for (uint i = 0; i < checkers.length; i++) {
+            checkersList[i] = checkers[i];
+            checkersVotes[i] = results[checkers[i]];
         }
     }
 
